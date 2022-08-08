a78e14e693973dd5f80ed56440006addad18c691
azeem-sarwar <azeemsarwarrockzs@gmail.com>
2022-08-08 17:18:25 +0500

    Remove extra comment

--- utility/case.ts
+++ utility/case.ts
@@ -1,29 +1,3 @@
-// export const toCamelCase = (str: string) => {
-//     return str
-//         .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, offset) =>
-//             offset === 0 ? word.toLowerCase() : word.toUpperCase()
-//         )
-//         .replace(/\s+/g, "");
-// };
-
-// export const toKebabCase = (str: string) => {
-//     return str.replace(
-//         /[A-Z]+(?![a-z])|[A-Z]/g,
-//         (word, offset) => (offset === 0 ? "" : "-") + word.toLowerCase()
-//     );
-// };
-
-// export const toPascalCase = (str: string) => {
-//     return `${str}`
-//         .replace(new RegExp(/[-_]+/, "g"), " ")
-//         .replace(new RegExp(/[^\w\s]/, "g"), "")
-//         .replace(
-//             new RegExp(/\s+(.)(\w*)/, "g"),
-//             ($1, $2, $3) => `${$2.toUpperCase() + $3}`
-//         )
-//         .replace(new RegExp(/\w/), (s) => s.toUpperCase());
-// };
-
 export const toCamelCase = (str: string) => {
     return str
         .replace(/(-|_)+/g, " ")
